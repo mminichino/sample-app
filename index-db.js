d75f61af82a4505b0dcff3cb7610e5278526027b
Michael Minichino <mminichino@mac.com>
2019-08-08 08:49:41 -0500

    New File

--- index-db.js
+++ index-db.js
@@ -27,9 +27,9 @@ app.get('/', function (req, res) {
 
 
 // server
-var server = app.listen(3000, function () {
+var server = app.listen(8080, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Sample App Running ... URL: http://%s:%s', host, port);
 });
