26a0752e4610830383186047db44ab2bdee1614b
Michael Minichino <mminichino@mac.com>
2019-08-08 11:47:13 -0500

    New File

--- index.js
+++ index.js
@@ -1,14 +1,74 @@
-var express = require('express');
-var app = express();
-var http = require('http');
+// A very basic web server in node.js
 
-app.get('/', function (req, res) {
-  res.render('index.html');
-});
+var port = 8080;
 
-var server = app.listen(8080, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+var http = require("http");
+var path = require("path");
+var fs = require("fs");
+var checkMimeType = true;
 
-  console.log('Sample App Running ... URL: http://%s:%s', host, port);
-});
+console.log("Starting web server on port " + port);
+
+http.createServer( function(req, res) {
+
+  var now = new Date();
+
+  var filename = req.url || "/index.html";
+  if (filename == '/') {
+    filename = "/index.html";
+  }
+  console.log("filename: " + filename)
+  var ext = path.extname(filename);
+  var localPath = __dirname;
+  var validExtensions = {
+    ".html" : "text/html",
+    ".js": "application/javascript",
+    ".css": "text/css",
+    ".txt": "text/plain",
+    ".jpg": "image/jpeg",
+    ".gif": "image/gif",
+    ".png": "image/png",
+    ".woff": "application/font-woff",
+    ".woff2": "application/font-woff2"
+  };
+
+  var validMimeType = true;
+  var mimeType = validExtensions[ext];
+  if (checkMimeType) {
+    validMimeType = validExtensions[ext] != undefined;
+  }
+
+  if (validMimeType) {
+    localPath += filename;
+    fs.exists(localPath, function(exists) {
+      if(exists) {
+        console.log("Serving file: " + localPath);
+        getFile(localPath, res, mimeType);
+      } else {
+        console.log("File not found: " + localPath);
+        res.writeHead(404);
+        res.end();
+      }
+    });
+
+  } else {
+    console.log("Invalid file extension detected: " + ext + " (" + filename + ")")
+  }
+
+}).listen(port);
+
+function getFile(localPath, res, mimeType) {
+  fs.readFile(localPath, function(err, contents) {
+    if(!err) {
+      res.setHeader("Content-Length", contents.length);
+      if (mimeType != undefined) {
+        res.setHeader("Content-Type", mimeType);
+      }
+      res.statusCode = 200;
+      res.end(contents);
+    } else {
+      res.writeHead(500);
+      res.end();
+    }
+  });
+}
