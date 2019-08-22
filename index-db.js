5e9efce5a109b0b29746592170e1c4c2a349dd7c
Michael Minichino <mminichino@mac.com>
2019-08-21 22:35:17 -0500

    Updated documentation

--- index-db.js
+++ index-db.js
@@ -8,6 +8,7 @@ var path = require("path");
 var fs = require("fs");
 var checkMimeType = true;
 var mysql = require("mysql");
+var useragent = require('express-useragent');
 
 if (process.env.APPVERSION != undefined) {
   var appversion = process.env.APPVERSION
@@ -50,7 +51,7 @@ http.createServer( function(req, res) {
     fs.exists(localPath, function(exists) {
       if(exists) {
         console.log("Serving file: " + localPath);
-        getFile(localPath, res, mimeType);
+        getFile(localPath, res, mimeType, req);
       } else {
         console.log("File not found: " + localPath);
         res.writeHead(404);
@@ -80,7 +81,7 @@ async function connectToDatabase(connection) {
   });
 }
 
-async function updateContents(contents) {
+async function updateContents(contents, req) {
   var connection = mysql.createConnection({ host: process.env.MYSQL_HOST,
                                                    user: process.env.MYSQL_USER,
                                                    password: process.env.MYSQL_PASSWORD,
@@ -98,6 +99,21 @@ async function updateContents(contents) {
       contents = contents.toString('utf8');
       contents = contents.replace("{{number}}", dbRes.insertId);
       contents = contents.replace("{{version}}", appversion);
+      contents = contents.replace("{{requestorip}}", req.connection.remoteAddress);
+      var source = req.headers['user-agent'],
+      ua = useragent.parse(source);
+      if (ua.isDesktop == true) {
+        var deviceType = "Desktop";
+      } else if (ua.isTablet == true) {
+        var deviceType = "Tablet";
+      } else {
+        var deviceType = "Mobile";
+      }
+      contents = contents.replace("{{devtype}}", deviceType);
+      contents = contents.replace("{{webbrowser}}", ua.browser + " " + ua.version);
+      contents = contents.replace("{{browseros}}", ua.os);
+      var podName = process.env.POD_NAME || "No Pod Name";
+      contents = contents.replace("{{podname}}", podName);
       contents = Buffer.from(contents, 'utf8');
       resolve(contents);
     });
@@ -105,7 +121,7 @@ async function updateContents(contents) {
   return data;
 }
 
-function getFile(localPath, res, mimeType) {
+function getFile(localPath, res, mimeType, req) {
   fs.readFile(localPath, function(err, contents) {
     if(!err) {
       if (mimeType != undefined) {
@@ -114,7 +130,7 @@ function getFile(localPath, res, mimeType) {
       if(mimeType == "text/html") {
         (async function f(){
           try {
-            contents = await updateContents(contents);
+            contents = await updateContents(contents, req);
           } catch(err) {
             res.writeHead(500);
             res.end(err);
