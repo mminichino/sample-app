73cb8b03f39ebdfcebb5aa24f00dd661f66c7e30
Michael Minichino <mminichino@mac.com>
2019-09-03 20:50:00 -0500

    Updates

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,34 +1,66 @@
 #!/bin/sh
 #
 FAIL=0
-TIMEOUT=50
+TIMEOUT=6000
 COUNT=1
 RETRY=5
 
-echo "Starting container..."
-docker run -d --name sampleapp-test -p 8080:8080 mminichino/sample-app node index-static.js
+echo "Starting DB container..."
+docker run -d --name sampleapp-test-db -e MYSQL_ROOT_PASSWORD='password' -e MYSQL_USER='sampleapp' -e MYSQL_PASSWORD='password' -e MYSQL_DATABASE='sampleapp' -p 3306:3306 mysql:5.7
 [ $? -ne 0 ] && exit 1
 
-CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sampleapp-test)
-if [ -z "$CONTAINER_IP" ]
+DB_CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sampleapp-test-db)
+if [ -z "$DB_CONTAINER_IP" ]
 then
    echo "Error can not get container IP"
+   docker stop sampleapp-test-db
+   docker rm sampleapp-test-db
    exit 1
 fi
 
-while ! nc -z $CONTAINER_IP 8080; do
+while ! nc -z $DB_CONTAINER_IP 3306; do
   COUNT=$(($COUNT+1))
   if [ $COUNT -gt $TIMEOUT ]
   then
      echo "Timeout"
+     docker logs sampleapp-test-db
+     docker stop sampleapp-test-db
+     docker rm sampleapp-test-db
+     exit 1
+  fi
+  sleep 0.1
+done
+
+COUNT=1
+echo "Starting App container..."
+docker run -d --name sampleapp-test -e MYSQL_HOST=$DB_CONTAINER_IP -e MYSQL_USER='sampleapp' -e MYSQL_PASSWORD='password' -e MYSQL_DATABASE='sampleapp' -p 8080:8080 mminichino/sample-app node index.js
+[ $? -ne 0 ] && exit 1
+
+APP_CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sampleapp-test)
+if [ -z "$APP_CONTAINER_IP" ]
+then
+   echo "Error can not get container IP"
+   docker stop sampleapp-test
+   docker rm sampleapp-test
+   exit 1
+fi
+
+while ! nc -z $APP_CONTAINER_IP 8080; do
+  COUNT=$(($COUNT+1))
+  if [ $COUNT -gt $TIMEOUT ]
+  then
+     echo "Timeout"
+     docker logs sampleapp-test
+     docker stop sampleapp-test
+     docker rm sampleapp-test
      exit 1
   fi
   sleep 0.1
 done
 
 while [ $RETRY -ge 0 ]; do
-curl http://${CONTAINER_IP}:8080 > /tmp/sampleapp.curl.test
-grep "Version: 1" /tmp/sampleapp.curl.test
+curl http://${APP_CONTAINER_IP}:8080 > /tmp/sampleapp.curl.test
+grep "visitor 1" /tmp/sampleapp.curl.test
 if [ $? -ne 0 ]
 then
    echo "Test Failed"
@@ -42,11 +74,20 @@ RETRY=$(($RETRY-1))
 sleep 1
 done
 
-echo "Stopping container..."
+echo "Stopping App container..."
 docker stop sampleapp-test
 [ $? -ne 0 ] && exit 1
-echo "Removing container..."
+
+echo "Stopping DB container..."
+docker stop sampleapp-test-db
+[ $? -ne 0 ] && exit 1
+
+echo "Removing App container..."
 docker rm sampleapp-test
+[ $? -ne 0 ] && exit 1
+
+echo "Removing DB container..."
+docker rm sampleapp-test-db
 [ $? -ne 0 ] && exit 1
 
 [ $FAIL -ne 0 ] && exit 1
